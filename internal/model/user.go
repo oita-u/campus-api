0a4e73ec7361f7c8c907683250dc019cd48f64aa
fog <38463346+fog-zs@users.noreply.github.com>
2025-12-02 15:29:30 +0900

    refactor(user): add db tags to User struct

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,8 +1,8 @@
 package model
 
 type User struct {
-	ID       string `json:"id"`
-	Email    string `json:"email"`
-	Password string `json:"-"` // 返さない
-	Name     string `json:"name"`
+	ID       string `db:"id" json:"id"`
+	Email    string `db:"email" json:"email"`
+	Password string `db:"password" json:"-"` // 返さない
+	Name     string `db:"name" json:"name"`
 }
