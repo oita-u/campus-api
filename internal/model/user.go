fe83b142885c69118cbcf19c90424f0db1213991
fog <38463346+fog-zs@users.noreply.github.com>
2025-12-02 15:30:41 +0900

    refactor: remove Japanese comment from User struct

--- internal/model/user.go
+++ internal/model/user.go
@@ -3,6 +3,6 @@ package model
 type User struct {
 	ID       string `db:"id" json:"id"`
 	Email    string `db:"email" json:"email"`
-	Password string `db:"password" json:"-"` // 返さない
+	Password string `db:"password" json:"-"`
 	Name     string `db:"name" json:"name"`
 }
