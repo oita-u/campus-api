b8489cf4b4ecfb73f3dbfe2d68ec50dac54f39c9
fog <38463346+fog-zs@users.noreply.github.com>
2025-12-02 16:39:19 +0900

    fix

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,11 +1,11 @@
 package model
 
 type User struct {
-	ID           int64  `db:"id" json:"id"`
-	UserCode     string `db:"user_code" json:"user_code"`
+	ID           string `db:"id" json:"id"`
 	Email        string `db:"email" json:"email"`
 	Password     string `db:"password" json:"-"`
 	Name         string `db:"name" json:"name"`
+	UserCode     string `db:"user_code" json:"user_code"`
 	Grade        string `db:"grade" json:"grade"`
 	DepartmentID int64  `db:"department_id" json:"department_id"`
 }
